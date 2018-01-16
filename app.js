adadd0764abf38e91cec6a0a7a7bd1ecf280dc78
Lindsay Maher <lindsaymah3r@gmail.com>
2018-01-16 10:10:47 -0500

    add middleware

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require( 'express');
+const volleyball = require('volleyball');
 const app = express();
+
+// app.use(function(req, res, next) {
+// 	console.log(res.statusCode);
+// 	console.log(req.method, req.path);
+// 	next()
+// })
+
+// app.use('/special', function(req, res, next) {
+// 	console.log('you reached the v special area');
+// 	next()
+// })
+
+app.use(volleyball);
 
 app.get('/', (req, res) => res.send("Our super SICK message."))
 
