8575489a9ecfe30d89040a4c67c4ec33453e7225
Lindsay Maher <lindsaymah3r@gmail.com>
2018-01-16 11:03:44 -0500

    add nunjucks rendering etc

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require( 'express');
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 const app = express();
 
 // app.use(function(req, res, next) {
@@ -13,7 +14,28 @@ const app = express();
 // 	next()
 // })
 
+var locals = {
+
+	title: "An Example",
+	
+	people: [
+
+	{name: "Gandolf"},
+	{name: "Frodo"},
+	{name: "Hermione"}
+
+	]
+}
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+
 app.use(volleyball);
+
+app.get('/', function(req, res){
+	res.render('index.html', locals);
+});
 
 app.get('/', (req, res) => res.send("Our super SICK message."))
 
